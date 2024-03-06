4e5d0e3adbbee3638de3a625615560562a05624c
Purich Seenuallae <purichseenuallae@gmail.com>
2024-03-06 19:15:07 +0700

    Add Link and handle search error

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import "../components/ProfileImages.jsx";
 import data from "../c60-data-query/data.js";
@@ -20,20 +21,22 @@ export default function Search({ searchInputValue }) {
     function extractNumbersFromString(text) {
         // Use a regular expression to match numbers in the text
         const numbers = text.match(/\d+/g);
-      
+
         // Check if numbers were found
         if (numbers) {
-          // Convert the matched strings to actual numbers
-          const numericValues = numbers.map(Number);
-          
-          return numericValues;
+            // Convert the matched strings to actual numbers
+            const numericValues = numbers.map(Number);
+
+            return numericValues;
         } else {
-          return [];
+            return [];
         }
-      }
+    }
 
     // save history of search to local storage
     const saveHistory = (searchInputValue) => {
+        console.log(searchInputValue);
+        console.log("save history");
         const history = JSON.parse(localStorage.getItem("history")) || [];
         history
             .filter((item) => item !== searchInputValue)
@@ -52,25 +55,25 @@ export default function Search({ searchInputValue }) {
         // Step 1: Find item frequencies
         const frequencyMap = {};
         for (const item of arr) {
-          frequencyMap[item] = (frequencyMap[item] || 0) + 1;
+            frequencyMap[item] = (frequencyMap[item] || 0) + 1;
         }
-      
+
         // Step 2: Sort by frequency (descending order)
         const sortedItems = Object.entries(frequencyMap).sort(([, countA], [, countB]) => countB - countA);
-      
+
         // Step 3: Extract unique items
         const uniqueItems = sortedItems.map(([item]) => item);
-      
+
         return uniqueItems;
-      }
-    
+    }
+
 
     // render history of search
     const renderHistory = () => {
         const history = getHistory();
         return history.map((item, index) => {
             return (
-                <div key={index} className="border-b border-gray-500 flex items-center">
+                <div key={index} className="border-b border-gray-500 flex items-center py-2">
                     <div className="pr-2">
                         <Icon icon="material-symbols:history" className="text-2xl" />
                     </div>
@@ -81,42 +84,49 @@ export default function Search({ searchInputValue }) {
     }
 
     const renderArticleResult = () => {
+        if (!articleResult) return;
         return articleResult.map((item, index) => {
+            
             return (
-                <div key={index} className="border-b border-gray-500 flex items-center">
-                    <div className="pr-2">
-                        <Icon icon="bx:bx-book" className="text-2xl" />
-                    </div>
-                    <span>{item}</span>
-                </div>
+                <Link to={"/section/" + item} >
+                        <div key={index} className="border-b border-gray-500 flex items-center">
+                            <div className="pr-2">
+                                <Icon icon="bx:bx-book" className="text-2xl" />
+                            </div>
+                            <span>{item}</span>
+                        </div>
+                </Link>
             );
         });
     }
 
     const renderDiscussionistResult = () => {
+        if (!discussionistResult) return;
         return discussionistResult.map((item, index) => {
             return (
-                <div key={index} className="border-b border-gray-500 flex items-center">
-                    <div className="pr-2">
-                        <Icon icon="bx:bx-user" className="text-2xl" />
+                <Link to={"/discussionist/" + item} >
+                    <div key={index} className="border-b border-gray-500 flex items-center">
+                        <div className="pr-2">
+                            <Icon icon="bx:bx-user" className="text-2xl" />
+                        </div>
+                        <span>{item}</span>
                     </div>
-                    <span>{item}</span>
-                </div>
+                </Link>
             );
         });
     }
 
     useEffect(() => {
         const articleSearch = createDataObject(data).filter('มาตรา', extractNumbersFromString(searchInputValue))
-        .append(createDataObject(data).search('มาตรา', searchInputValue))
-        .append(createDataObject(data).search('ผู้อภิปราย', searchInputValue))
-        .append(createDataObject(data).search('ประเด็นการพิจารณา', searchInputValue))
-        .append(createDataObject(data).search('ร่างบทบัญญัติ', searchInputValue))
-        setArticleResult([...new Set(articleSearch.data.map(obj => obj.มาตรา))].splice(0,5));
+            .append(createDataObject(data).search('มาตรา', searchInputValue))
+            .append(createDataObject(data).search('ผู้อภิปราย', searchInputValue))
+            .append(createDataObject(data).search('ประเด็นการพิจารณา', searchInputValue))
+            .append(createDataObject(data).search('ร่างบทบัญญัติ', searchInputValue))
+        setArticleResult([...new Set(articleSearch.data.map(obj => obj.มาตรา))].splice(0, 5));
 
         // discussionist
         // search by name
-        let discussionistNameSearch =[...new Set(createDataObject(data)
+        let discussionistNameSearch = [...new Set(createDataObject(data)
             .data
             .map(obj => obj.ผู้อภิปราย)
             .flat())]
@@ -124,13 +134,13 @@ export default function Search({ searchInputValue }) {
 
         // search by others
         let discussionistOthersSearch = sortAndExtractUniqueByFrequency(createDataObject(data).search('มาตรา', searchInputValue)
-        .append(createDataObject(data).search('ประเด็นการพิจารณา', searchInputValue))
-        .append(createDataObject(data).search('ร่างบทบัญญัติ', searchInputValue))
-        .data
-        .map(obj => obj.ผู้อภิปราย)
-        .flat());
-        
-        setDiscussionistResult([...new Set(discussionistNameSearch.concat(discussionistOthersSearch))].splice(0,5));
+            .append(createDataObject(data).search('ประเด็นการพิจารณา', searchInputValue))
+            .append(createDataObject(data).search('ร่างบทบัญญัติ', searchInputValue))
+            .data
+            .map(obj => obj.ผู้อภิปราย)
+            .flat());
+
+        setDiscussionistResult([...new Set(discussionistNameSearch.concat(discussionistOthersSearch))].splice(0, 5));
 
     }, [searchInputValue]);
 
@@ -141,13 +151,13 @@ export default function Search({ searchInputValue }) {
         <div data-testid="search">
             <div className="bg-[#1a1a1a] h-screen">
                 <div className="max-w-screen-md mx-auto text-xl text-white pt-4" >
-                    
+
                     {searchInputValue ? (
                         <div>
                             <h4>
                                 ค้นหาตามมาตรา
                             </h4>
-                            
+
                             {renderArticleResult()}
 
                             <h4 className="pt-4">
