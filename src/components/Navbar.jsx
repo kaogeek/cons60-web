18dd4d70caa8009b7935c09c9ec8870813e6627a
Anurat Eiamphoklarp <rhymerealm@gmail.com>
2024-03-12 02:51:39 +0700

    #58 fix top nav on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -126,7 +126,15 @@ function Navbar({ setSearchInputValue }) {
                         to="/"
                         className="text-white hover:underline underline-offset-4"
                       >
-                        ภาพรวม
+                        หน้าหลัก
+                      </Link>
+                    </div>
+                    <div className="w-auto inline-block">
+                      <Link
+                        to="/search"
+                        className="text-white hover:underline underline-offset-4"
+                      >
+                        ค้นหา
                       </Link>
                     </div>
                     <div className="w-auto inline-block">
