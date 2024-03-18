189c26747a42c9d2d425450e73eb8437a26556f0
Anurat Eiamphoklarp <rhymerealm@gmail.com>
2024-03-19 06:38:52 +0700

    #142 add new line between volunteers name and last name in about page

--- src/components/ProfileImages.jsx
+++ src/components/ProfileImages.jsx
@@ -1,26 +1,22 @@
 const ProfileImages = ({ name, imageUrl, size }) => {
-    return (
-        <div className="text-center">
-          <div className="rounded-full overflow-hidden h-20 w-20 bg-[#222] border-[3px] border-[#ddd] mx-auto"
-                style={{ height: size, width: size }}
-            >
-              <img className="object-cover h-full w-full" src={imageUrl} alt={'ภาพถ่ายของ ' + name} />
-            </div>
-            {
-              name
-                ? <div className="mt-3 text-header">
-                    {
-                      name
-                        .split(' ')
-                        .map(x => (
-                          <><span className="inline-block">{x}</span> </>
-                        ))
-                    }
-                  </div>
-                : null
-            }
+  const nameArr = name?.split(' ');
+  return (
+    <div className="text-center">
+      <div
+        className="rounded-full overflow-hidden h-20 w-20 bg-[#222] border-[3px] border-[#ddd] mx-auto"
+        style={{ height: size, width: size }}
+      >
+        <img className="object-cover h-full w-full" src={imageUrl} alt={'ภาพถ่ายของ ' + name} />
+      </div>
+      { nameArr && (
+        <div className="mt-3 text-header">
+          {nameArr[0]}
+          <br />
+          {nameArr[1]}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default ProfileImages;
