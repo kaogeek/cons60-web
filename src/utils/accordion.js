b21a8e1fbad9817dff30b05e0cc0494adb0cd27b
Anurat Eiamphoklarp <rhymerealm@gmail.com>
2024-03-22 03:40:17 +0700

    #147 fix bug when click pdf link will collapse itself if active

--- src/utils/accordion.js
+++ src/utils/accordion.js
@@ -1,21 +1,24 @@
-export default function accordionToggle(id) {
+export default function accordionToggle(id, forceOpen=false) {
   
   const navActive = document.querySelector('.nav-active');
-  const accordionExpand = document.getElementById('accordion-'+id);
+  const accordionClick = document.getElementById('accordion-'+id);
   const accordionActive = document.querySelector('.accordion-active');
   const accordionActiveID = accordionActive?.dataset.accordionId;
-  
-  accordionActive?.classList.remove('accordion-active');
-  accordionActive?.classList.add('accordion-collapsed');
-  if (accordionActiveID != id) {
-    accordionExpand.classList.remove('accordion-collapsed');
-    accordionExpand.classList.add('accordion-active');
-    accordionExpand.scrollIntoView({ behavior: 'smooth' });
+
+  if (!forceOpen || accordionActiveID != id) {
+    accordionActive?.classList.remove('accordion-active');
+    accordionActive?.classList.add('accordion-collapsed');
   }
   
-  const navExpand = document.getElementById('nav-'+id);
+  if (accordionActiveID != id) {
+    accordionClick.classList.remove('accordion-collapsed');
+    accordionClick.classList.add('accordion-active');
+    accordionClick.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  const navExpanded = document.getElementById('nav-'+id);
   navActive.className = 'nav py-2 px-5 font-bold text-left w-full md:w-64 text-[#9F9F9F]';
-  navExpand.className = 'nav nav-active py-2 px-5 font-bold text-left w-full md:w-64 bg-white rounded-lg text-[#131313]';
+  navExpanded.className = 'nav nav-active py-2 px-5 font-bold text-left w-full md:w-64 bg-white rounded-lg text-[#131313]';
     
   return;
   
